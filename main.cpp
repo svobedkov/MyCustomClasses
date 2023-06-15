b4162b3e6ae0315dc2d1777c6d913720a5011ba5
svobedkov <svobedkov@edu.hse.ru>
2023-06-16 01:06:35 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -4,10 +4,43 @@
 int main_menu();
 
 int stack_f();
+int queue_f();
 
 template<typename T>
 int stack_int() {
     stack<T> my_stack;
+    int var;
+    while (true) {
+        system("cls");
+        std::cout << "Stack:" << std::endl;
+        my_stack.map();
+        std::cout << std::endl;
+        std::cout << "Choose container:" << std::endl << "1.Push" << std::endl << "2.Pop" << std::endl << "3.Clear" << std::endl << "0.Exit" << std::endl;
+        std::cin >> var;
+        std::cin.clear();
+        std::cin.ignore();
+        switch (var) {
+            case 0:
+                return 0;
+            case 1:
+                T x;
+                std::cout << "Input: ";
+                std::cin >> x;
+                my_stack.push(x); // CHECK THIS
+                break;
+            case 2:
+                my_stack.pop();
+                break;
+            case 3:
+                my_stack.clear();
+                break;
+        }
+    }
+}
+
+template<typename T>
+int queue_int() {
+    queue<T> my_stack;
     int var;
     while (true) {
         system("cls");
@@ -47,6 +80,7 @@ int main() {
                 stack_f();
                 break;
             case 2:
+                queue_f();
                 break;
             case 3:
                 break;
@@ -94,3 +128,31 @@ int stack_f() {
     }
 }
 
+int queue_f() {
+    system("cls");
+    int temp;
+    while (true) {
+        system("cls");
+        std::cout << "Choose type of data:" << std::endl << "1.int" << std::endl << "2.char" << std::endl << "3.double" << std::endl << "0.Exit" << std::endl;
+        std::cin >> temp;
+        system("cls");
+        std::cin.clear();
+        std::cin.ignore();
+        switch (temp) {
+            case 0:
+                return 0;
+            case 1:
+                queue_int<int>();
+                break;
+            case 2:
+                queue_int<char>();
+                break;
+            case 3:
+                queue_int<double>();
+                break;
+            default:
+                std::cout << "WRONG INPUT" << std::endl;
+                break;
+        }
+    }
+}
